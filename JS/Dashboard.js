bc7b2a7c05c636f963ea42d5785b06e3bf221db0
Hamza Y Alzoubi <hamzaalzoubi94@gmail.com>
2024-12-28 09:44:28 -0800

    more fixed < now is ok

--- JS/Dashboard.js
+++ JS/Dashboard.js
@@ -30,7 +30,7 @@ document.onclick = function() {
 let Logout = document.querySelector(".Logout");
 
 Logout.onclick = function () {
-    location.href = "http://127.0.0.1:5500/index.html"
+    location.href = "https://hamzayalzoubi.github.io/Novo-Team-Maneger/"
 }
 /* End Hedaer */
 
